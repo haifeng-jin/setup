667c08725d85c27c5f38c9b5cf349f731600a6ba
Haifeng Jin <haifeng-jin@users.noreply.github.com>
2024-09-24 23:15:53 -0700

    vim

---
+++ vim.sh
@@ -0,0 +1,8 @@
+# Download my vim config
+git clone https://github.com/haifeng-jin/vim-config.git .vim
+
+# Download the vim plugin manager
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install the plugins
+vim +PlugInstall +qa
