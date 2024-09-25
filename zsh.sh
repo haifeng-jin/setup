8c4e33a85bcb9efc6abc2f212c38e000eb4b67e6
Haifeng Jin <haifeng-jin@users.noreply.github.com>
2024-09-24 23:06:43 -0700

    zsh

--- zsh.sh
+++ zsh.sh
@@ -5,10 +5,12 @@ sudo apt install zsh
 sudo apt install wget
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
-# Setup theme to pure
+# Install theme to pure
 mkdir -p "$HOME/.zsh"
 git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
 
-echo "fpath+=($HOME/.zsh/pure)" >> .zshrc
-echo "autoload -U promptinit; promptinit" >> .zshrc
-echo "prompt pure" >> .zshrc
+# Install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Copy the zshrc
+cp zshrc ~/.zshrc
