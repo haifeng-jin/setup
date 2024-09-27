0e829fb30205118aeb9bbb08d3f5cc35f1f464b4
Haifeng Jin <haifeng-jin@users.noreply.github.com>
2024-09-27 00:02:15 -0700

    update tmux

--- tmux.sh
+++ tmux.sh
@@ -3,3 +3,9 @@ sudo apt install tmux
 
 # Copy the config
 cp tmux.conf ~/.tmux.conf
+
+# Install the TMUX plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Install the plugins
+~/.tmux/plugins/tpm/bin/install_plugins
